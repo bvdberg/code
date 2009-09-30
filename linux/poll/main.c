d28bf81518908407f21f38bc936de5f5e98ea1ba
bvdberg <b.van.den.berg.nl@gmail.com>
2009-09-30 12:59:13 +0200

    several connections

--- linux/poll/main.c
+++ linux/poll/main.c
@@ -7,8 +7,13 @@
 #include <sys/socket.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
+
 
 static struct sockaddr_in sin;
+static struct pollfd fds[20];
+static int count = 0;
+static int stop = 0;
 
 int open_socket(int port) {
     sin.sin_family = AF_INET;
@@ -45,10 +50,82 @@ int open_socket(int port) {
 }
 
 
+void printConnections() {
+    int i;
+    printf("%s()\n", __func__);
+    for (i=0; i<count; i++) {
+         printf(" %2d\n", fds[i].fd);
+    }
+}
+
+void addConnection(int fd, short flags) {
+    fds[count].fd = fd;
+    fds[count].events = flags;
+    count++;
+    printConnections();
+}
+
+void acceptConnection(int fd) {
+    struct sockaddr_in remote;
+    unsigned int sin_size = sizeof(struct sockaddr_in);
+    int newfd = accept(fd,(struct sockaddr *) &remote, &sin_size);
+    if (newfd == -1) {
+        printf("%s() ERROR\n", __func__);
+        return;
+    }              
+    printf("received connection %d from %s\n", newfd, inet_ntoa(remote.sin_addr));
+    addConnection(newfd, POLLIN);
+}
+
+void removeConnection(int index) {
+    int fd = fds[index].fd;
+    close(fd);
+    int i;
+    for (i=index; i<count-1; i++) {
+        printf("copying %d -> %d\n", i, i+1);
+        fds[i].fd = fds[i+1].fd;
+        fds[i].events = fds[i+1].events;
+        fds[i].revents = fds[i+1].revents;
+    }
+    count--;
+    printf("lost connection %d\n", fd);
+    printConnections();
+}
+
+void sigint(int num) {
+    stop = 1;
+}
+
 int main(int argc, const char *argv[])
 {
     int fd = open_socket(6666);
     if (fd == -1) return 0;
+
+    addConnection(fd, POLLIN);
+    int timeout_ms = 500;
+
+    signal(SIGINT, sigint);
+    while (stop == 0) {
+        int ret = poll(fds, count, timeout_ms);
+        if (ret != 0) printf("ret = %d\n", ret);
+        if (fds[0].revents & POLLIN) {
+            acceptConnection(fds[0].fd);
+        }
+        int i;
+        for (i=1; i<count; i++) {
+            if (fds[i].revents & POLLIN) {
+                char buffer[64];
+                int bytes = read(fds[i].fd, buffer, 64);
+                if (bytes <= 0) { 
+                    removeConnection(i);
+                } else {
+                    buffer[bytes] = 0;
+                    buffer[bytes-2] = 0;    // strip off CR/LF
+                    printf("%2d: %d bytes - '%s'\n", fds[i].fd, bytes, buffer);
+                }
+            }
+        }
+    }
 
     close(fd);
 
