d6b74ca15f60d1d655ad5316037fe5f54b2c2238
bvdberg <b.van.den.berg.nl@gmail.com>
2010-02-11 07:51:26 +0100

    added input type

---
+++ linux/stdin_type/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+// Determine if stdin/stdout are pipes, files or normal terminals
+// ./test < Makefile
+// ./test < /dev
+
+int main(int argc, const char *argv[])
+{
+    // 0=stdin, 1=stdout, 2=stderr
+    struct stat buf;
+    int error = fstat(0, &buf);
+    if (error) {
+        perror("fstat");
+        return -1;
+    }
+    mode_t m = buf.st_mode;
+    printf("stdin: regular=%d  chardev=%d  blockdev=%d  fifo=%d  dir=%d\n",
+        S_ISREG(m), S_ISCHR(m), S_ISBLK(m), S_ISFIFO(m), S_ISDIR(m));
+
+    return 0;
+}
+
