62babc613f59f9c2ecd7fd0edad2d002e8447ebb
bvdberg <b.van.den.berg.nl@gmail.com>
2010-03-31 10:19:47 +0200

    printing whole file

--- linux/mmap/main.cpp
+++ linux/mmap/main.cpp
@@ -14,15 +14,24 @@ int main(int argc, char* argv[]) {
     int fd = open(argv[1], O_RDONLY);
     if (fd == -1) { perror("open"); return -1; }
 
-    int pagesize = getpagesize();
-    void* region = mmap(0, pagesize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_POPULATE, fd, 0);
+    struct stat statbuf;
+    int err = stat(argv[1], &statbuf);
+    if (err) {
+        perror("stat");
+        return -1;
+    }
+    printf("size = %d\n", statbuf.st_size);
+    //int pagesize = getpagesize();
+    //void* region = mmap(0, pagesize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_POPULATE, fd, 0);
+    void* region = mmap(0, statbuf.st_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_POPULATE, fd, 0);
     if (region == MAP_FAILED) { perror("mmap"); return -1; }
+    close(fd);
 
     printf("region = %p\n", region);
-    printf("%s", region);
+    printf("%s\n", region);
 
-    munmap(region, pagesize);
-    close(fd);
+    //munmap(region, pagesize);
+    munmap(region, statbuf.st_size);
 
     return 0;
 }
