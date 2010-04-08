d129b505e0bc9652626dd49cc20bcbf56a203dcf
bvdberg <b.van.den.berg.nl@gmail.com>
2010-04-08 09:04:07 +0200

    main

--- ansi-c/strip_whitespace/main.c
+++ ansi-c/strip_whitespace/main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/*
+ *  strip whitespace from file:
+ *  - empty lines
+ *  - lines ending with spaces + newline
+ *  - support both newline chars
+ */
+
 int main(int argc, const char *argv[])
 {
     
