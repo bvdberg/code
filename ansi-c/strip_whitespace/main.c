b2813e18d9f9d54acbb61fc3feb184da68bd725a
bvdberg <b.van.den.berg.nl@gmail.com>
2010-04-08 10:27:38 +0200

    print lines

--- ansi-c/strip_whitespace/main.c
+++ ansi-c/strip_whitespace/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <string.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <unistd.h>
@@ -15,9 +16,53 @@
  */
 
 
-void parseFile(char* file, unsigned int size)
+#define LF 10
+#define CR 13
+#define TAB 0x09
+#define SPACE 0x20
+
+#define ANSI_BRED "\033[01;31m"
+#define ANSI_NORMAL "\033[0m"
+#define ANSI_BGRED "\33[0;37;40m"
+
+static void checkLine(int lineNr, char* line, char* end)
 {
-    printf("%s\n", file);
+    int size = end - line;
+    if (size == 0) {
+        //printf("empty line: [%d]\n", lineNr);
+        return;
+    }
+    char* cp = end-1;   
+    while (cp != line) {  
+        if (*cp != TAB && *cp != SPACE) break;
+        cp--;
+    }
+    int white = (end-1) - cp;
+    if (white) {
+        char normal[256];
+        char space[256];
+        memset(normal, 0, sizeof(normal));
+        strncpy(normal, line, cp+1-line);        
+        strncpy(space, cp+1, end-(cp+1));
+        printf("[%3d] %s"ANSI_BGRED"%s"ANSI_NORMAL"\n", lineNr, normal, space);
+    }
+}
+
+
+static void parseFile(char* file, unsigned int size)
+{
+    int lineNr = 1;
+    char* line = file;
+    char* cp = line;
+    while (cp - file < size) {
+        if (*cp == LF || *cp == CR) {
+            checkLine(lineNr, line, cp);
+            lineNr++;
+            line = cp+1;
+            // TODO read next enters
+        }
+        cp++;
+    }
 }
 
 int main(int argc, const char *argv[])
