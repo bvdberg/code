e4c4917cf8a9017c438dade4f30ddab3072a54da
bvdberg <b.van.den.berg.nl@gmail.com>
2010-05-21 10:52:55 +0200

    added Duff's device

---
+++ ansi-c/duff/duff.c
@@ -0,0 +1,35 @@
+// Duff's device
+#include <stdio.h>
+
+int func(int count)
+{
+    int a = 10;
+    int* to = &a;
+    int* from = &a;
+    switch (count % 8) {
+    case 0:
+        do {
+            *to = *from++;
+        case 7:
+            *to = *from++;
+        case 6:
+            *to = *from++;
+        case 5:
+            *to = *from++;
+        case 4:
+            *to = *from++;
+        case 3:
+            *to = *from++;
+        case 2:
+            *to = *from++;
+        case 1:
+            *to = *from++;
+        } while ((count -= 8) > 0);
+    }
+}
+int main(int argc, const char *argv[])
+{
+    func(92);
+    return 0;
+}
+
