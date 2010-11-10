bd8c509a9ae23b232151e1d951f3b1e80eb072d0
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2010-11-10 10:59:04 +0100

    working

--- linux/syslog/main.c
+++ linux/syslog/main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <syslog.h>
 
 int main(int argc, const char *argv[])
 {
+    openlog("my user log", 0, LOG_USER);
+
+    syslog(LOG_ALERT, "my user alert");
+    syslog(LOG_CRIT, "my user crit");
+    syslog(LOG_ERR, "my user err");
+    syslog(LOG_WARNING, "my user warning");
+    syslog(LOG_NOTICE, "my user notice");
+    syslog(LOG_INFO, "my user info");
+    syslog(LOG_DEBUG, "my user debug");
+
+    closelog();
+
     return 0;
 }
 
