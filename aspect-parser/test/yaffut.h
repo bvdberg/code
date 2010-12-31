8fed95b4913ad43a68bdbd9f356420c4ed155486
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2010-12-31 10:58:54 +0100

    updated yaffut

--- aspect-parser/test/yaffut.h
+++ aspect-parser/test/yaffut.h
@@ -12,27 +12,14 @@
 #include <iostream>
 #include <map>
 #include <memory>
+#include <limits>
 #include <sstream>
 #include <stdexcept>
 #include <sys/time.h>
+#include <string.h>
+#include <stdlib.h>
 
-#define ANSI_BLACK "\033[22;30m"
-#define ANSI_RED "\033[22;31m"
-#define ANSI_GREEN "\033[22;32m"
-#define ANSI_BROWN "\033[22;33m"
-#define ANSI_BLUE "\033[22;34m"
-#define ANSI_MAGENTA "\033[22;35m"
-#define ANSI_CYAN "\033[22;36m"
-#define ANSI_GREY "\033[22;37m"
-#define ANSI_DARKGREY "\033[01;30m"
-#define ANSI_BRED "\033[01;31m"
-#define ANSI_BGREEN "\033[01;32m"
-#define ANSI_YELLOW "\033[01;33m"
-#define ANSI_BBLUE "\033[01;34m"
-#define ANSI_BMAGENTA "\033[01;35m"
-#define ANSI_BCYAN "\033[01;36m"
-#define ANSI_WHITE "\033[01;37m"
-#define ANSI_NORMAL "\033[22;37m"
+#include "color.h"
 #define COLOR_AT     ANSI_YELLOW
 #define COLOR_OK     ANSI_GREEN
 #define COLOR_FAIL   ANSI_BRED
@@ -88,7 +75,7 @@ std::string demangle()
   size_t sz;
   int status;
   char* ptr = abi::__cxa_demangle(typeid(T).name(), 0, &sz, &status);
-  std::string name(ptr ? ptr : "", ptr ? strlen(ptr) : 0);
+  std::string name(ptr ? ptr : "", (ptr ? strlen(ptr) : 0));
   if(ptr){ free(ptr); }
   std::string::size_type pos = name.rfind("::");
   if(pos != std::string::npos)
