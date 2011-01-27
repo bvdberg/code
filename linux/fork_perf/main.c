8f7a36eeb0be96ce3680666beea3955f502db691
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2011-01-27 13:16:54 +0100

    added pthread

--- linux/fork_perf/main.c
+++ linux/fork_perf/main.c
@@ -4,6 +4,13 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <pthread.h>
+
+#ifdef USE_PTHREAD
+void* thread_main(void* arg) {
+    pthread_exit(0);
+}
+#endif
 
 u_int64_t getNow()
 {
@@ -20,6 +27,7 @@ int main(int argc, const char *argv[])
     int runs = 10000;
     int current = 0;
 
+#ifdef USE_FORK
     u_int64_t t1 = getNow();
     while (current < runs) {
         pid_t pid = fork();
@@ -42,6 +50,19 @@ int main(int argc, const char *argv[])
     }
     u_int64_t t2 = getNow();
     printf("%d forks in %llu usec, %llu usec/fork\n", runs, t2-t1, (t2-t1)/runs);
+#endif
+
+#ifdef USE_PTHREAD
+    u_int64_t t1 = getNow();
+    while (current < runs) {
+        pthread_t thread;
+        pthread_create(&thread, 0, thread_main, 0);
+        pthread_join(thread, 0);
+        current++;
+    }
+    u_int64_t t2 = getNow();
+    printf("%d pthreads in %llu usec, %llu usec/fork\n", runs, t2-t1, (t2-t1)/runs);
+#endif
     return 0;
 }
 
