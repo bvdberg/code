3f0b247b892304068c1f13ab8a658c29abdae172
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2011-02-25 09:16:21 +0100

    added local function

---
+++ ansi-c/local_function/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+typedef int (*func_ptr)(int a, int b);
+
+void run_function(func_ptr func) {
+    func(2, 5);
+}
+
+void select_function(int num) {
+    int function_a(int a , int b)
+    {
+        printf("%s() \n", __func__);
+        return a+b;
+    }
+    int function_b(int a , int b)
+    {
+        printf("%s() \n", __func__);
+        return a*b;
+    }
+    func_ptr func = function_a;
+    if (num > 5) func = function_b;
+    run_function(func);
+}
+
+
+int main()
+{
+    select_function(10);
+    select_function(2);
+    return 0;
+}
+
