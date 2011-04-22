a0d66093e06832f66505b405868bc0d9f2c3b5ad
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2011-04-22 10:59:17 +0200

    added print hex()

---
+++ ansi-c/printHex/main.c
@@ -0,0 +1,29 @@
+// print hex value manually (for embedded targets)
+#include <stdio.h>
+
+void uart_putc(char c)
+{
+    printf("%c", c);
+}
+
+
+char hexvalues[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+
+void printHex(unsigned int value)
+{
+    int i;
+    for (i=28; i>= 0; i-= 4) {
+        unsigned char hex = (value >> i) & 0xF;
+        uart_putc(hexvalues[hex]);
+    }
+}
+
+int main(int argc, const char *argv[])
+{
+    int num = 123456;
+    printHex(num);    
+    printf("\n");
+    fflush(stdout);
+    printf("TEST %d -> 0x%08x\n", num, num);
+    return 0;
+}
