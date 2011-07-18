541037f230248f433ed4303c3bebac3778e6038b
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2011-07-18 12:02:57 +0200

    more options

--- linux/datetime/main.c
+++ linux/datetime/main.c
@@ -7,12 +7,25 @@
 #include <unistd.h>
 #include <stdint.h>
 
+#if 0 
+int tm_sec;         /* seconds */
+int tm_min;         /* minutes */
+int tm_hour;        /* hours */
+int tm_mday;        /* day of the month */
+int tm_mon;         /* month */
+int tm_year;        /* year */
+int tm_wday;        /* day of the week */
+int tm_yday;        /* day in the year */
+int tm_isdst;       /* daylight saving time */
+#endif
+
 const char* getTime() {
-    static char buffer[10];
+    static char buffer[40];
     time_t now = time(0);
     struct tm* now2 = localtime(&now);
-    //struct tm* now2 = gmtime(&now);
-    sprintf(buffer, "%02d:%02d:%02d", now2->tm_hour, now2->tm_min, now2->tm_sec);
+    sprintf(buffer, "%d-%02d-%02d %02d:%02d:%02d",
+        now2->tm_year + 1900, now2->tm_mon + 1, now2->tm_mday,
+        now2->tm_hour, now2->tm_min, now2->tm_sec);
     return buffer;
 }
 
@@ -27,7 +40,6 @@ int main() {
 
     printf("ctime: %s", ctime(&now));
  
-    
     struct tm* now2 = gmtime(&now);
     printf("gmtime: %02d:%02d:%02d\n", now2->tm_hour, now2->tm_min, now2->tm_sec);
 
@@ -35,7 +47,7 @@ int main() {
     strftime(buffer, 20, "%A %p", now2); 
     printf("strftime (variable): %s\n", buffer);
 
-    printf("now = %s\n", getTime());
+    printf("custom = %s\n", getTime());
     return 0;
 }
 
