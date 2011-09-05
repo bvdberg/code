8b21468f232d5764c385762bc23ba36b8a70cfdc
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2011-09-05 14:34:03 +0200

    working scan

--- cpp/zipfile_reader/main.cpp
+++ cpp/zipfile_reader/main.cpp
@@ -15,49 +15,52 @@
 #include <zzip/zzip.h>
 #endif
 
+void checkFile(ZZIP_DIR* dir, ZZIP_DIRENT* dirent)
+{
+    int flags = 0;
+    ZZIP_FILE* f1 = zzip_file_open(dir, dirent->d_name, flags);
+    if (f1 == NULL) {
+        fprintf(stderr, "zzip_file_open failed: %s\n", zzip_strerror_of(dir));
+        return;
+    }
+
+    // ---- file info ----
+    char buf[4096];
+    zzip_ssize_t size = zzip_file_read(f1, buf, 4096);
+    if (size != dirent->st_size) {
+        fprintf(stderr, "only read %d bytes\n", size);
+        return;
+    }
+    //printf("%s\n", buf);
+    //printf("---------------------\n");
+
+    int err = zzip_file_close(f1);
+    if (err != 0) {
+        fprintf(stderr, "zzip_file_close failed: %s\n", zzip_strerror_of(dir));
+        return;
+    }
+}
+
+
 int main(int argc, const char *argv[])
 {
     ZZIP_DIR* dir = zzip_opendir("bd1");
     if (dir == NULL) {
-        fprintf(stderr, "zzip_operdir failed\n");
+        fprintf(stderr, "zzip_operdir failed: TODO (errno?)\n");
         return -1;
     }
 
-    int flags = 0;
-    const char* name = "data";
-    ZZIP_FILE* f1 = zzip_file_open(dir, name, flags);
-    if (f1 == NULL) {
-        fprintf(stderr, "zzip_file_open failed: %s\n", zzip_strerror_of(dir));
-        return -1;
+    // scan files
+    ZZIP_DIRENT* dirent = zzip_readdir(dir);
+    while (dirent) {
+        printf("name %s, compression %d, size %d/%d\n",
+            dirent->d_name, dirent->d_compr, dirent->d_csize, dirent->st_size);
+
+        checkFile(dir, dirent);
+        dirent = zzip_readdir(dir);
     }
 
-    // ---- file info ----
-    ZZIP_STAT stat;
-    int err = zzip_file_stat(f1, &stat);
-    if (err != 0) {
-        fprintf(stderr, "zzip_file_stat failed: %s\n", zzip_strerror_of(dir));
-        return -1;
-    }
-    printf("Stat: compr=%d,  csize=%d,  usize=%d  name=%s\n",
-        stat.d_compr, stat.d_csize, stat.st_size, stat.d_name);
-
-    printf("------ FILE %s size %d/%d ------\n", name, stat.d_csize, stat.st_size);
-    char buf[4096];
-    zzip_ssize_t size = zzip_file_read(f1, buf, 4096);
-    if (size != stat.st_size) {
-        fprintf(stderr, "only read %d bytes\n", size);
-        return -1;
-    }
-    printf("%s\n", buf);
-    printf("---------------------\n");
-
-    err = zzip_file_close(f1);
-    if (err != 0) {
-        fprintf(stderr, "zzip_file_close failed: %s\n", zzip_strerror_of(dir));
-        return -1;
-    }
-
-    err = zzip_closedir(dir);
+    int err = zzip_closedir(dir);
     if (err != 0) {
         fprintf(stderr, "zzip_closedir failed: %s\n", zzip_strerror_of(dir));
         return -1;
@@ -65,3 +68,4 @@ int main(int argc, const char *argv[])
 
     return 0;
 }
+
