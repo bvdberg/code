1cb661bec928a229d4a0439f101305ad4102036b
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2011-11-22 07:58:46 +0100

    circbuf: adding unit tests

--- ansi-c/circular_buffer/tester.cpp
+++ ansi-c/circular_buffer/tester.cpp
@@ -18,7 +18,35 @@ struct CircBufTest {
 };
 
 
-TEST(CircBufTest, testNumber) {
+TEST(CircBufTest, testEmpty) {
+    ASSERT_EQUAL(0, buffer_data(&buf));
+    ASSERT_EQUAL(BUF_SIZE-1, buffer_free(&buf));
 }
 
 
+TEST(CircBufTest, testAddingOneItem) {
+    buffer_add(&buf, 1);
+    ASSERT_EQUAL(1, buffer_data(&buf));
+    ASSERT_EQUAL(BUF_SIZE-2, buffer_free(&buf));
+}
+
+
+TEST(CircBufTest, testAddingAndConsumingOne) {
+    for (int i=0; i<BUF_SIZE+4; i++) {
+        buffer_add(&buf, 1);
+        buffer_remove(&buf, 1);
+        ASSERT_EQUAL(0, buffer_data(&buf));
+        ASSERT_EQUAL(BUF_SIZE-1, buffer_free(&buf));
+    }
+}
+
+
+TEST(CircBufTest, testAddingAndConsumingOneOffsetOne) {
+    buffer_add(&buf, 1);
+    for (int i=0; i<BUF_SIZE+4; i++) {
+        buffer_add(&buf, 1);
+        buffer_remove(&buf, 1);
+        ASSERT_EQUAL(1, buffer_data(&buf));
+        ASSERT_EQUAL(BUF_SIZE-2, buffer_free(&buf));
+    }
+}
