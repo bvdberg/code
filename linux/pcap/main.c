f8811cc33030f0321deb69777a67a83732158653
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2012-01-19 09:51:33 +0100

    rtptracker: filter IP / UDP

--- linux/pcap/main.c
+++ linux/pcap/main.c
@@ -13,9 +13,21 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netinet/if_ether.h>
+#include <netinet/ip.h>
+#include <netinet/in.h>
+
+static char* ip2str(unsigned int addr) {
+    static char buffer[4][20];
+    static int index = 0;
+    unsigned char* a = (unsigned char*)&addr;
+    index++;
+    if (index == 4) index = 0;
+    char* cp = buffer[index];
+    sprintf(cp, "%d.%d.%d.%d", a[0], a[1], a[2], a[3]);
+    return cp;
+}
 
 static int count = 0;
 void receive_cb(u_char *useless, const struct pcap_pkthdr* pkthdr, const u_char* packet)
@@ -25,9 +37,15 @@ void receive_cb(u_char *useless, const struct pcap_pkthdr* pkthdr, const u_char*
         printf("weird sizes!\n");
         exit(-1);
     }
-    printf("[%5d] %d bytes  len=%d\n", count, pkthdr->caplen, pkthdr->len);
-    //struct pcap_pkthdr hdr;     /* pcap.h */
     //struct ether_header *eptr;  /* net/ethernet.h */
+    struct ether_header* eptr = (struct ether_header*)packet;
+    unsigned int type = ntohs(eptr->ether_type);
+    if (type != 0x800) return;  // NO ip
+    struct iphdr* hdr = (struct iphdr*)(&eptr[1]);
+    if (hdr->protocol != IPPROTO_UDP) return; // NO UDP
+    printf("[%5d] %4d bytes  ", count, pkthdr->caplen);
+    printf("src=%15s  dst=%15s  protocol=%d\n", ip2str(ntohl(hdr->saddr)), ip2str(ntohl(hdr->daddr)), hdr->protocol);
+
 }
 
 int main(int argc,char **argv)
