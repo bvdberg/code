b715b03f9a0f402cda7482846f6005994d7071ff
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2012-01-23 12:06:46 +0100

    added longjmp example

---
+++ ansi-c/jump/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <setjmp.h>
+
+void longjmp(jmp_buf env, int val);
+
+void siglongjmp(sigjmp_buf env, int val);
+
+static jmp_buf env;
+static jmp_buf env2;
+
+int function2(char n) {
+    printf("function2(%c)\n", n);
+    if (n == 'D') longjmp(env, 5);
+    if (n == 'F') longjmp(env2, 6);
+    printf("returning from function2(%c)\n", n);
+    return 15;
+}
+
+
+void function1() {
+    char i;
+    for (i='A'; i<'H'; i++) {
+        printf("----- %c ----\n", i);
+        int res = setjmp(env);
+        if (res != 0) {
+            printf("coming from jump ONE, res=%d\n", res);
+            continue;
+        }
+        res = setjmp(env2);
+        if (res != 0) {
+            printf("coming from jump TWO, res=%d\n", res);
+            continue;
+        }
+        usleep(250000);
+
+        res = function2(i);
+    }
+}
+
+int main(int argc, const char *argv[])
+{
+    function1();
+    
+    return 0;
+}
+
