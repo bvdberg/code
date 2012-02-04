357745039e984a888b2aab5fc0a6f8a109f0c150
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2012-02-04 07:43:00 +0100

    wildcards: done

--- linux/wildcards/main.c
+++ linux/wildcards/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+// run with ./test /somedir/*.c
+
 int main(int argc, char* argv[]) {
+    int i;
+    for (i=0; i<argc; i++) printf("arg[%d] = '%s'\n", i, argv[i]);
 
     return 0;
 }
