c0342383b2ed3f24d0767ec7d2bad3f7ee909bef
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2012-03-19 12:16:34 +0100

    cleanup

--- linux/ubi_parser/create_user.sh
+++ linux/ubi_parser/create_user.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#+MKUBIFS_ARGS = "-m 1 -e 130944 -c 192"
-#+UBINIZE_ARGS = "-m 1 -p 128KiB"
-
 UBIFS_IMG=user.ubifs.img
 UBI_IMG=user.ubi
-rm -f $UBI_IMG $UBIFS_IMG
-mkfs.ubifs -r image_root -o $UBIFS_IMG -m 1 -e 130944 -c 764
+ROOT_DIR=image_root
+USER_SIZE=764
+rm -rf $UBI_IMG $UBIFS_IMG $ROOT_DIR
+mkdir $ROOT_DIR
+mkfs.ubifs -r $ROOT_DIR -o $UBIFS_IMG -m 1 -e 130944 -c $USER_SIZE
 ubinize -o $UBI_IMG -m 1 -p 128KiB ubinize.cfg
 
