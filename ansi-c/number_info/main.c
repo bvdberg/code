9b22c4288d6a7bd42b9e0f06bce576b2f5c063f2
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2012-04-02 12:23:45 +0200

    ninfo

--- ansi-c/number_info/main.c
+++ ansi-c/number_info/main.c
@@ -4,16 +4,21 @@
 
 static u_int64_t total;
 static u_int64_t count;
+static int min = 2000000000;
+static int max;
 
 int main(int argc, const char *argv[])
 {
     count = argc - 1;
     unsigned int i;
     for (i=1; i<=count; i++) {
-        total += atoi(argv[i]);
+        int num = atoi(argv[i]);
+        total += num;
+        if (num < min) min = num;
+        if (num > max) max = num;
     }
     double total_d = total;
-    printf("total %llu  total=%llu  avg=%f\n", count, total, total_d / count);
+    printf("%llu numbers:  min=%d  max=%d  sum=%llu  avg=%f\n", count, min, max, total, total_d / count);
     return 0;
 }
 
