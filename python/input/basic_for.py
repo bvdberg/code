a0126f1dc4ee62f927e9a14b0f2727a4e5be20c1
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2012-04-10 14:44:11 +0200

    python: more examples

--- python/input/basic_for.py
+++ python/input/basic_for.py
@@ -3,3 +3,12 @@
 a = ['bla', 'bliep', 'blop']
 for x in a:
     print x, len(x)
+
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print n, 'equals', x, '*', n/x
+            break
+    else:
+        print n, 'is a prime number'
+
---
+++ python/input/basic_range.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+a = ['Mary', 'had', 'a', 'little', 'lamb']
+for i in range(len(a)):
+    print i, a[i]
