bdcaadff7e5486a769b464d0b6eb2f672a47a4d9
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2012-07-18 12:05:43 +0200

    udpserver: temp

--- linux/udp-client-server/UdpServer.cpp
+++ linux/udp-client-server/UdpServer.cpp
@@ -1,5 +1,6 @@
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <string.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
@@ -10,6 +11,34 @@
 #include <stdexcept>
 
 using namespace std;
+static void print_line(const unsigned char* buffer,  int offset, int num) {
+    static char hexval[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+    int i;
+    unsigned char* hex_cp;
+    unsigned char* char_cp;
+    unsigned char line[80];
+    memset(line, ' ', sizeof(line));
+    line[79] = 0;
+    hex_cp = &line[0];
+    char_cp = &line[16 * 3 + 4];
+    for (i=0; i< num; i++) {
+        unsigned char input = buffer[offset + i];
+        *hex_cp++ = hexval[input >> 4];
+        *hex_cp++ = hexval[input & 0x0F];
+        hex_cp++;
+        *char_cp++ = (isprint(input) ? input : '.');
+    }
+    printf("%s\n", line);
+}
+
+static void print_buffer(const unsigned char* buffer, int buffer_len) {
+    int offset = 0;
+    while (offset < buffer_len) {
+        int remaining = buffer_len - offset;
+        print_line(buffer, offset, (remaining >= 16) ? 16 : remaining);
+        offset += 16;
+    }
+}
 
 UdpServer::UdpServer(unsigned int port) {
     if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
@@ -25,6 +54,7 @@ UdpServer::UdpServer(unsigned int port) {
         close(fd);
         throw std::logic_error("cannot bind to socket");
     }
+    printf("listening to port %d\n", port);
 }
 
 
@@ -44,14 +74,14 @@ void UdpServer::run() {
             throw std::logic_error("cannot receive");
         }
 
-        checker.addPacket(msgbuf, recv_bytes);
-        checker.getStatus();
+        printf("received %d bytes from %s\n", recv_bytes, inet_ntoa(cliAddr.sin_addr));
+        print_buffer((const unsigned char*)msgbuf, recv_bytes);
     }
 }
 
 
 int main() {
-    UdpServer server(12345);
+    UdpServer server(6701);
     server.run();
 
     return 0;
