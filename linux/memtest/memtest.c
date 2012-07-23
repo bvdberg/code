d952ecad8193e8eb096dbb391b87887380bc4ac5
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2012-07-23 13:09:40 +0200

    memtest: fix

--- linux/memtest/memtest.c
+++ linux/memtest/memtest.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 #include <stdlib.h>
 
 int main(int argc, char* argv[])
@@ -13,13 +15,15 @@ int main(int argc, char* argv[])
 	int i;
 	for (i=0; i<times; i++) {
 		total += amount;
-		void* buffer = malloc(amount*1024);
+		void* buffer = calloc(amount, 1024);
 		if (buffer == NULL) {
 			printf("alloc failed\n");
 			break;
 		}
+        memset(buffer, 0, amount*1024);
 		printf("allocated %d kB, total %8d kB\n", amount, total);
 	}
+    sleep (20);
 
 	return 0;
 }
