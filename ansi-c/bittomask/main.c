20318fc4768cbc0dd107e2b993aeb758e0ce0d2e
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2014-08-26 15:44:00 +0200

    improved getmask

--- ansi-c/bittomask/main.c
+++ ansi-c/bittomask/main.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <assert.h>
 
+static unsigned getMask(unsigned first, unsigned last) {
+    unsigned mask = 0xffffffff;
+    mask >>= (31-last);
+
+    if (first != 0) {
+        unsigned mask2 = 0xffffffff;
+        mask2 >>= (32-first);
+        mask -= mask2;
+    }
+    return mask;
+}
+
 static void printMask(unsigned first, unsigned last) {
     assert(first <= last);
-    unsigned mask = 0;
-    for (unsigned i=first; i<=last; i++) {
-        mask |= (1<<i);
-    }
-    printf("Mask for [%d:%d] = 0x%04x\n", first, last, mask);
+    printf("Mask for [%2d:%2d] = 0x%04x\n", first, last, getMask(first, last));
 }
 
 int main(int argc, const char *argv[])
@@ -16,6 +24,7 @@ int main(int argc, const char *argv[])
     printMask(0, 4);
     printMask(2, 2);
     printMask(2, 4);
+    printMask(3, 4);
     printMask(0, 15);
 
     return 0;
