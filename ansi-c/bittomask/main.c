e43725ce261434a84e874b3968db29de46f1d9fc
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2014-08-26 16:03:12 +0200

    added 64bit bittomask

--- ansi-c/bittomask/main.c
+++ ansi-c/bittomask/main.c
@@ -1,31 +1,55 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <assert.h>
 
-static unsigned getMask(unsigned first, unsigned last) {
-    unsigned mask = 0xffffffff;
+static uint32_t getMask32(uint32_t first, uint32_t last) {
+    assert(first <= last);
+    uint32_t mask = (uint32_t)-1;
     mask >>= (31-last);
 
     if (first != 0) {
-        unsigned mask2 = 0xffffffff;
+        uint32_t mask2 = (uint32_t)-1;
         mask2 >>= (32-first);
         mask -= mask2;
     }
     return mask;
 }
 
-static void printMask(unsigned first, unsigned last) {
+static uint64_t getMask64(uint64_t first, uint64_t last) {
     assert(first <= last);
-    printf("Mask for [%2d:%2d] = 0x%04x\n", first, last, getMask(first, last));
+    uint64_t mask = (uint64_t)-1;
+    mask >>= (63-last);
+
+    if (first != 0) {
+        uint64_t mask2 = (uint64_t)-1;
+        mask2 >>= (64-first);
+        mask -= mask2;
+    }
+    return mask;
+}
+
+static void printMask32(uint32_t first, uint32_t last) {
+    printf("Mask32 for [%2d:%2d] = 0x%04x\n", first, last, getMask32(first, last));
+}
+
+static void printMask64(uint64_t first, uint64_t last) {
+    printf("Mask64 for [%2d:%2d] = 0x%04llx\n", (int)first, (int)last, getMask64(first, last));
 }
 
 int main(int argc, const char *argv[])
 {
-    printMask(0, 0);
-    printMask(0, 4);
-    printMask(2, 2);
-    printMask(2, 4);
-    printMask(3, 4);
-    printMask(0, 15);
+    printMask32(0, 0);
+    printMask32(0, 4);
+    printMask32(2, 2);
+    printMask32(2, 4);
+    printMask32(3, 4);
+    printMask32(0, 15);
+    printf("\n");
+    printMask64(0, 0);
+    printMask64(6, 7);
+    printMask64(31, 31);
+    printMask64(63, 63);
+
 
     return 0;
 }
