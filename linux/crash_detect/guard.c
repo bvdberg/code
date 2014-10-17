2af12b813ecb3d55952b0f39ac381b87fcfc3063
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2014-10-17 11:55:26 +0200

    crash_detect: redirect stdout/stdin to files

--- linux/crash_detect/guard.c
+++ linux/crash_detect/guard.c
@@ -11,7 +11,11 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdlib.h>
-
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <time.h>
 
 typedef struct {
     pid_t pid;
@@ -20,17 +24,71 @@ typedef struct {
 
 #define MAX_ARGS 20
 
-static Child start_app(const char* prog, char* argv[]) {
+static void writeChecked(int fd, const char* data) {
+    size_t len = strlen(data);
+    int numwritten = write(fd, data, len);
+    if (numwritten != len) {
+
+    }
+}
+
+static void error(const char* msg) {
+    int fd = open("error.log", O_CREAT | O_WRONLY, 0644);
+    if (fd == -1) return;
+    writeChecked(fd, msg);
+    close(fd);
+    exit(-1);
+}
+
+static Child start_app(const char* prog, char* argv[], int redirect) {
     Child child;
     strncpy(child.executable, prog, 63);
     pid_t child_pid = fork();
     if (child_pid == 0) { //child
         usleep(50000);
+        int fdout = -1;
+        int fderr = -1;
+        if (redirect) {
+            fflush(stdout);
+            close(1);
+            fdout = open("./out.log", O_APPEND | O_CREAT | O_WRONLY, 0644);
+            if (fdout == -1) {
+                error("open(out)");
+            }
+
+#if 1
+            // same file
+            close(2);
+            fderr = dup(1);
+            if (fderr  == -1) {
+                error("dup() returned error");
+            }
+#else
+            // different file
+            fflush(stderr);
+            close(2);
+            fderr = open("./err.log", O_APPEND | O_CREAT | O_WRONLY, 0644);
+            if (fderr == -1) {
+                error("open(err)");
+            }
+#endif
+            fprintf(stderr, "---- [%ld] starting %s ----\n", time(0), prog);
+            fflush(stderr);
+            //writeChecked(1, "--- NEW OUTPUT ---\n");
+            //writeChecked(2, "--- NEW ERROR ---\n");
+            // NOTE printf's on stdout are sometimes not seen in resulting file (exec does not flusH)
+            //syncfs(1);
+            //syncfs(2);
+        }
         //execvp(argv[1], NULL);
         //execlp(prog, prog, (char*)NULL);
         execv(prog, argv);
-        printf("guard: error starting %s\n", prog);
         child.pid = 0;
+        fprintf(stderr, "---- [%ld] failed to start: %s: %s\n", time(0), prog, strerror(errno));
+        fflush(stderr);
+        close(fdout);
+        close(fderr);
+        error("GUARD: error starting child\n");
     } else {   //parent
         printf("guard: started [%s] - pid %d\n", prog, child_pid);
         child.pid = child_pid;
@@ -47,7 +105,7 @@ int main(int argc, char *argv[])
     }
 
     //phase 1 - start child process
-    Child child = start_app(argv[1], &argv[1]);
+    Child child = start_app(argv[1], &argv[1], 1);
 
     int count = 0;
     while (count < 4) {
@@ -59,7 +117,7 @@ int main(int argc, char *argv[])
         if (!WIFEXITED(status)) { //child exits abnormally
             if (child.pid == child_pid) {
                 printf("guard: process [%s] (pid %d) crashed!\n", child.executable, child.pid);
-                child = start_app(argv[1], &argv[1]);
+                child = start_app(argv[1], &argv[1], 1);
             }
             // else weird
         } else {
--- linux/crash_detect/main.c
+++ linux/crash_detect/main.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(int argc, const char *argv[])
 {
-    printf("application started, %d args\n", argc);
+    time_t now = time(0);
+    fprintf(stdout, "application: test output %ld\n", now);
+    fprintf(stderr, "application: test error %ld\n", now);
+/*
     int i;
     for (i=0; i<argc; i++) printf("arg[%d] = '%s'\n", i, argv[i]);
+*/
     sleep(4);
 #ifdef CRASH
+    printf("crash\n");
     abort();  // to cause crash
 #endif
     printf("end\n");
