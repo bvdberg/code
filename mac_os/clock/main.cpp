3522d9bfb0afef1695282b0a1824020431367c43
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2014-11-06 22:02:26 +0100

    mac_os clocks: gettimeofday test

--- mac_os/clock/main.cpp
+++ mac_os/clock/main.cpp
@@ -1,21 +1,39 @@
 #include <stdio.h>
 #include <inttypes.h>
 #include <unistd.h>
+#include <sys/time.h>
 
 #include <mach/mach.h>
 #include <mach/mach_time.h>
+
+static uint64_t getCurrentTime() {
+    struct timeval tp;
+    if (gettimeofday(&tp, NULL) == -1) {
+        perror("gettimeofday");
+        return 0;
+    }
+    uint64_t result = tp.tv_sec;
+    result *= 1000000;
+    result += tp.tv_usec;
+    return result;
+}
 
 int main(int argc, const char *argv[])
 {
     uint64_t start = mach_absolute_time();
     usleep(100000);
     uint64_t end = mach_absolute_time();
-    printf("100usec => diff  %lld nsec\n", end - start);
+    printf("100msec => diff  %lld nsec\n", end - start);
 
     uint64_t start2 = mach_absolute_time();
     sleep(1);
     uint64_t end2 = mach_absolute_time();
     printf("1sec    => diff %lld nsec\n", end2 - start2);
+
+    uint64_t now = getCurrentTime();
+    usleep(100000);
+    uint64_t now2 = getCurrentTime();
+    printf("100ms => diff %lld usec\n", now2 - now);
     return 0;
 }
 
