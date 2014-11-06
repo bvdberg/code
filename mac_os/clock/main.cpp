b2c199b662c97824c4dae802ef4deab0bbb176ec
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2014-11-06 21:52:27 +0100

    update

--- mac_os/clock/main.cpp
+++ mac_os/clock/main.cpp
@@ -10,12 +10,12 @@ int main(int argc, const char *argv[])
     uint64_t start = mach_absolute_time();
     usleep(100000);
     uint64_t end = mach_absolute_time();
-    printf("100usec => diff=%lld nsec\n", end - start);
+    printf("100usec => diff  %lld nsec\n", end - start);
 
     uint64_t start2 = mach_absolute_time();
     sleep(1);
     uint64_t end2 = mach_absolute_time();
-    printf("1sec => diff=%lld nsec\n", end2 - start2);
+    printf("1sec    => diff %lld nsec\n", end2 - start2);
     return 0;
 }
 
