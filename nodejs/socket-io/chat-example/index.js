27d5f5b6b6259ecc291489356b63395bd05adef3
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2018-09-27 10:41:49 +0200

    nodejs: added socket.io example

---
+++ nodejs/socket-io/chat-example/index.js
@@ -0,0 +1,25 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    //console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(8080, function(){
+  console.log('listening on *:8080');
+});
