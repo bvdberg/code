90e2b847bbe811e2343188b073e35c00164b962a
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2020-01-04 13:55:06 +0100

    cleanup

--- linux/tcp_connect_timeout/main.c
+++ linux/tcp_connect_timeout/main.c
@@ -13,38 +13,31 @@
 #include <time.h>
 
 int main(int argc, char **argv) {
-    struct sockaddr_in addr_s;
-    char *addr;
-    short int fd=-1;
-    int port;
-    fd_set fdset;
-    struct timeval tv;
-    int rc;
     int so_error;
-    socklen_t len;
-    struct timespec tstart={0,0}, tend={0,0};
-    int seconds;
+    socklen_t len = sizeof(so_error);
 
     if (argc != 4) {
         fprintf(stderr, "Usage: %s <ip> <port> <timeout_seconds>\n", argv[0]);
         return 1;
     }
 
-    addr = argv[1];
-    port = atoi(argv[2]);
-    seconds = atoi(argv[3]);
+    char* addr = argv[1];
+    int port = atoi(argv[2]);
+    int seconds = atoi(argv[3]);
 
+    struct sockaddr_in addr_s;
     addr_s.sin_family = AF_INET; // utilizzo IPv4
     addr_s.sin_addr.s_addr = inet_addr(addr);
     addr_s.sin_port = htons(port);
 
+    struct timespec tstart={0,0}, tend={0,0};
     clock_gettime(CLOCK_MONOTONIC, &tstart);
 
-    fd = socket(AF_INET, SOCK_STREAM, 0);
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
     fcntl(fd, F_SETFL, O_NONBLOCK); // setup non blocking socket
 
     // make the connection
-    rc = connect(fd, (struct sockaddr *)&addr_s, sizeof(addr_s));
+    int rc = connect(fd, (struct sockaddr *)&addr_s, sizeof(addr_s));
     if ((rc == -1) && (errno != EINPROGRESS)) {
         fprintf(stderr, "Error: %s\n", strerror(errno));
         close(fd);
@@ -62,8 +55,10 @@ int main(int argc, char **argv) {
         // connection attempt is in progress
     } */
 
+    fd_set fdset;
     FD_ZERO(&fdset);
     FD_SET(fd, &fdset);
+    struct timeval tv;
     tv.tv_sec = seconds;
     tv.tv_usec = 0;
 
@@ -71,10 +66,7 @@ int main(int argc, char **argv) {
     rc = select(fd + 1, NULL, &fdset, NULL, &tv);
     switch(rc) {
     case 1: // data to read
-        len = sizeof(so_error);
-
         getsockopt(fd, SOL_SOCKET, SO_ERROR, &so_error, &len);
-
         if (so_error == 0) {
             clock_gettime(CLOCK_MONOTONIC, &tend);
             printf("socket %s:%d connected. It took %.5f seconds\n",
