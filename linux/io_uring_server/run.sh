e854947a811e99ff21f9807d6667bf7b22fd0263
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2024-08-26 06:54:47 +0200

    add server arg

--- linux/io_uring_server/run.sh
+++ linux/io_uring_server/run.sh
@@ -1,12 +1,13 @@
-mate-terminal --geometry 86x14+50+50 --command "./client" --title "Client1"
-mate-terminal --geometry 86x14+800+50 --command "./client" --title "Client2"
-mate-terminal --geometry 86x14+1550+50 --command "./client" --title "Client3"
-mate-terminal --geometry 86x14+50+350 --command "./client" --title "Client4"
-mate-terminal --geometry 86x14+800+350 --command "./client" --title "Client5"
-mate-terminal --geometry 86x14+1550+350 --command "./client" --title "Client6"
-mate-terminal --geometry 86x14+50+650 --command "./client" --title "Client7"
-mate-terminal --geometry 86x14+800+650 --command "./client" --title "Client8"
-mate-terminal --geometry 86x14+1550+650 --command "./client" --title "Client9"
-mate-terminal --geometry 86x14+50+950 --command "./client" --title "Client10"
+SERVER=192.168.178.18
+mate-terminal --geometry 86x14+50+50 --command "./client ${SERVER}" --title "Client1"
+mate-terminal --geometry 86x14+800+50 --command "./client ${SERVER}" --title "Client2"
+mate-terminal --geometry 86x14+1550+50 --command "./client ${SERVER}" --title "Client3"
+mate-terminal --geometry 86x14+50+350 --command "./client ${SERVER}" --title "Client4"
+mate-terminal --geometry 86x14+800+350 --command "./client ${SERVER}" --title "Client5"
+mate-terminal --geometry 86x14+1550+350 --command "./client ${SERVER}" --title "Client6"
+mate-terminal --geometry 86x14+50+650 --command "./client ${SERVER}" --title "Client7"
+mate-terminal --geometry 86x14+800+650 --command "./client ${SERVER}" --title "Client8"
+mate-terminal --geometry 86x14+1550+650 --command "./client ${SERVER}" --title "Client9"
+mate-terminal --geometry 86x14+50+950 --command "./client ${SERVER}" --title "Client10"
 
 #./test
